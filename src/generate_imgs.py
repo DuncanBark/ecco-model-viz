46c739c5838cff06a9a540ba8d1318ae34753310
Kevin Marlis <kevinmarlis@gmail.com>
2020-11-05 11:49:33 -0800

    fixed gif file order

--- src/generate_imgs.py
+++ src/generate_imgs.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 
 def main(start='2000-01', end='2010-01', loc='', data_name='SEA_SURFACE_HEIGHT'):
     if not loc:
-        loc = Path(f'{Path(__file__).resolve().parents[2]}/model_output/{data_name}')
+        loc = Path(
+            f'{Path(__file__).resolve().parents[2]}/model_output/{data_name}')
 
     dates_in_range = np.arange(f'{start}', f'{end}', dtype='datetime64[M]')
 
@@ -28,27 +29,32 @@ def main(start='2000-01', end='2010-01', loc='', data_name='SEA_SURFACE_HEIGHT')
     counter = 0
     for datafile in files_to_use:
         counter += 1
-        if counter < 2:
+        if counter < 20:
             image_name = f'{image_output}{os.path.basename(datafile)[:-2]}png'
             if not os.path.exists(image_name):
                 fig = plt.figure()
                 ds = xr.open_dataset(datafile)
                 data_var = ds.SSH
 
-                plt.imshow(data_var[0], projection='')
+                plt.imshow(data_var[0])
                 plt.gca().invert_yaxis()
                 plt.gca().set_axis_off()
-                fig.savefig(image_name, dpi=150, bbox_inches='tight', pad_inches=0)
+                fig.savefig(image_name, dpi=150,
+                            bbox_inches='tight', pad_inches=0)
 
                 plt.close()
 
     # Create GIF
-    # images_to_use = os.listdir(image_output)
-    # images = []
-    # for img in images_to_use:
-    #     img = f'{image_output}{img}'
-    #     images.append(imageio.imread(img))
-    # imageio.mimsave(f'{image_output}animation.gif', images)
+    images_to_use = [f for f in os.listdir(image_output) if 'png' in f]
+    images_to_use.sort()
+    images = []
+    print(images_to_use)
+    for img in images_to_use:
+        img = f'{image_output}{img}'
+        print(img)
+        images.append(imageio.imread(img))
+    imageio.mimsave(f'{image_output}animation.gif', images)
+
 
 if __name__ == "__main__":
     main()
